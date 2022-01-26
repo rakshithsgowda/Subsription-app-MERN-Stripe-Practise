fc3e70fdc44acba357acaee32c4ca8bba318ad5a
rakshith <rakshit.s.gowda@gmail.com>
2022-01-26 19:32:25 +0530

    reusable form input component

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+import Input from '../components/Input'
+
 const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className='d-flex justify-content-center' style={{ height: '60vh' }}>
       <div className='container d-flex align-item-center'>
@@ -7,6 +14,22 @@ const Register = () => {
           <p className='lead pb-4'>
             Sign up for free No Credit Card required..!!
           </p>
+
+          <div className='form-group'>
+            <Input label='Name' value={name} setValue={setName} />
+            <Input
+              label='Email'
+              type='email'
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              label='Password'
+              type='password'
+              value={password}
+              setValue={setPassword}
+            />
+          </div>
         </div>
       </div>
     </div>
